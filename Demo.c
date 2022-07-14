e1f92dfc611e4205a5136ecb72c0e1e183160182
CodingMaster1310 <100837651+CodingMaster1310@users.noreply.github.com>
2022-07-14 14:39:29 +0530

    Add files via upload

---
+++ Demo.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+//void product(int num)
+//{
+//    int temp=num;
+//    int mul=1;
+//
+//    while(temp!=0)
+//    {
+//        int rem = temp%10;
+//        mul*=rem;
+//
+//        int div = temp/10;
+//        temp=div;
+//    }
+//    printf("The product of %d is : %d\n",num,mul);
+//}
+
+
+//void time_calculate(float distance, float ship_speed, float water_speed )
+//{
+//    float time = distance \ (ship_speed + water_speed);
+//    printf("Time is : %d\n",time);
+//}
+
+//void reverseNum(int num)
+//{
+//    while(num!=0)
+//    {
+//        int rem = num % 10;
+//        int div = num/10;
+//
+//        num = div;
+//
+//    }
+//
+//    printf("%d",rem);
+//}
+int main()
+{
+ //   product(1245443);
+//    product(254);
+//    product(907);
+
+      //reverseNum(254);
+      //time_calculate(100,5,5);
+
+//      float distance=100;
+//      float ship_speed=5;
+//      float water_speed=5;
+//
+//      float res = (ship_speed + water_speed);
+//
+//      float current_time = distance \ res;
+//
+//      printf("Time is : %d\n",current_time);
+
+
+    return 0;
+}
